e570ec6e25ebb0a80a44dd48fc18659c1bf0ea2d
ThiagoAugusto1806 <thiagoaugusto1806@gmail.com>
2024-10-09 19:49:39 -0300

    Iniciando projeto Spring

---
+++ src/main/java/com/Screenmatch/Screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,27 @@
+package com.Screenmatch.Screenmatch;
+
+import com.Screenmatch.Screenmatch.model.DadosSerie;
+import com.Screenmatch.Screenmatch.service.ConsumoAPI;
+import com.Screenmatch.Screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoAPI = new ConsumoAPI();
+		var json = consumoAPI.obterDados("http://www.omdbapi.com/?t=gilmore+girls&apikey=fe69c902");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+
+	}
+}
---
+++ src/test/java/com/Screenmatch/Screenmatch/ScreenmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Screenmatch.Screenmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
